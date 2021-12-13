0615486c02269cbb0a67c18bdf544d283b6cb86c
lucaslomba <lucasazevedolomba@gmail.com>
2021-12-13 15:25:23 -0300

    Criando botão de novo produto

--- crud/frontend/src/app/app.module.ts
+++ crud/frontend/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { MatSidenavModule } from '@angular/material/sidenav'
 import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component'
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { RedDirective } from './directives/red.directive';
 import { ProductCreateComponent } from './componenents/product/product-create/product-create.component'
@@ -35,7 +36,8 @@ import { ProductCreateComponent } from './componenents/product/product-create/pr
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
