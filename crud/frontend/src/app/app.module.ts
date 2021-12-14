c2b3fe44d103dc7c55f8c235d361b58acf6fd9bd
lucaslomba <lucasazevedolomba@gmail.com>
2021-12-14 13:37:25 -0300

    Exibindo dados do banco na tabela

--- crud/frontend/src/app/app.module.ts
+++ crud/frontend/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -22,7 +22,14 @@ import { ProductCreateComponent } from './componenents/product/product-create/pr
 import { HttpClientModule } from '@angular/common/http'
 import { FormsModule } from '@angular/forms'
 import { MatFormFieldModule } from '@angular/material/form-field'
-import { MatInputModule } from '@angular/material/input'
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { ProductReadComponent } from './componenents/product/product-read/product-read.component'
+
+import localePt from '@angular/common/locales/pt'
+import { registerLocaleData } from '@angular/common'  
+
+registerLocaleData(localePt)
 
 @NgModule({
   declarations: [
@@ -33,7 +40,8 @@ import { MatInputModule } from '@angular/material/input'
     HomeComponent,
     ProductCrudComponent,
     RedDirective,
-    ProductCreateComponent
+    ProductCreateComponent,
+    ProductReadComponent
   ],
   imports: [
     BrowserModule,
@@ -48,9 +56,13 @@ import { MatInputModule } from '@angular/material/input'
     HttpClientModule,
     FormsModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatTableModule
   ],
-  providers: [],
+  providers: [{
+    provide: LOCALE_ID,
+    useValue: 'pt-BR'
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
